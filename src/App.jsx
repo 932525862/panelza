2f93a6091a01f9a145c72a60c0c9c87a0b790f21
LimsaCompany <limsauzcompany@gmail.com>
2025-07-23 15:58:57 +0500

    add fotos page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
-import StickyContactBar from "./components/stickyContactBar/StickyContactBar"
 import Contact from "./pages/contact"
+import Fotos from "./pages/fotos"
 import Home from "./pages/home"
 import OurStory from "./pages/ourStory"
 import Products from "./pages/products"
 import RecentProjects from "./pages/projects"
-import Responsibility from "./pages/responsibility"
 import 'animate.css';
 import { Toaster } from 'sonner'
 
@@ -19,9 +18,8 @@ function App() {
       <Products/>
       <OurStory/>
       <RecentProjects/>
+      <Fotos/>
       <Contact/>
-      {/* <Responsibility/> */}
-      {/* <StickyContactBar/> */}
       <Footer/>
       <Toaster position="top-center" richColors />
     </>
