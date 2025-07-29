0b6f04028de3f52abf4c09fdcf749f0c44c0fb8a
LimsaCompany <limsauzcompany@gmail.com>
2025-07-29 10:02:14 +0500

    change hame page title in same info

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -13,17 +13,15 @@ function Home() {
       <div className="container relative z-20 ">
         <div className=" max-w-full min-[1030px]:max-w-[60%] max-[900px]:pt-[60px] text-center min-[1030px]:text-start absolute text-shadow-xs text-shadow-amber-500 flex flex-col max-[1030px]:items-center max-[1030px]:pt-[200px]">
           <h1 className=" text-white max-[1030px]:text-[50px] max-[670px]:text-[40px] max-[550px]:text-4xl min-[900px]:max-w-[70%] min-[1030px]:max-w-full text-5xl font-medium">
-            Завод по производству сэндвич-панелей с доставкой по
-            <span className="text-[#df6500]"> всему Казахстану</span>
+            Завод по производству сэндвич-панелей с доставкой по 
+            <span className="text-[#df6500]"> всему Узбекистану</span>
           </h1>
           <p className="text-white max-[550px]:text-2xl max-[1030px]:text-4xl max-[1030px]:text-[3xl] text-2xl  mt-7 min-[900px]:max-w-[70%] min-[1030px]:max-w-full">
-            Мы предлагаем широкий ассортимент сэндвич-панелей для различных
-            строительных нужд. Наши панели отличаются высоким качеством и
-            долговечностью.
+            Мы производим и поставляем сэндвич-панели высокого качества для различных строительных проектов по всей территории Узбекистана
           </p>
           <p className=" text-[#d3d3d3] text-xl max-[550px]:text-xl max-[1030px]:text-3xl max-[670px]:text-2xl mt-7 min-[900px]:max-w-[70%] min-[1030px]:max-w-full">
-            Мы предлагаем широкий ассортимент сэндвич-панелей для различных
-            строительных нужд
+           Широкий ассортимент для любых строительных задач
+Наши панели отличаются прочностью, отличной теплоизоляцией и длительным сроком службы. Предлагаем индивидуальные решения под нужды каждого клиента — от складов и ангаров до коммерческих и промышленных объектов.
           </p>
           {/* <div className="flex items-center text-shadow-xs text-shadow-amber-500">
             <a
